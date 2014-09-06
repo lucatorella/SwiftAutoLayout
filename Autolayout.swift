83efc36a37d768bcccfec3ae9f4ffd04274c9b37
Luca Torella <luca@blinkbox.com>
2014-09-06 22:11:19 +0200

    NSLayoutConstraint extension

--- Autolayout.swift
+++ Autolayout.swift
@@ -28,7 +28,7 @@ extension UIView {
         let superview: UIView? = self.superview
         assert(superview != nil, "View's superview must not be nil")
         assert(axis == .CenterX || axis == .CenterY, "You can center a view horizontally or vertically, no other attribute is accepted")
-        return NSLayoutConstraint(item: self, attribute: axis, relatedBy: .Equal, toItem: superview, attribute: axis, multiplier: 1, constant: 0)
+        return NSLayoutConstraint(item: self, attribute: axis, toItem: superview)
     }
 
     // MARK: Pin Edges to Superview
@@ -40,18 +40,18 @@ extension UIView {
     func pinEdgesToSuperview(distance: CGFloat) -> [NSLayoutConstraint] {
         let superview: UIView? = self.superview
         assert(superview != nil, "View's superview must not be nil")
-        let left = NSLayoutConstraint(item: self, attribute: .Left, relatedBy: .Equal, toItem: superview, attribute: .Left, multiplier: 1, constant: distance)
-        let right = NSLayoutConstraint(item: self, attribute: .Right, relatedBy: .Equal, toItem: superview, attribute: .Right, multiplier: 1, constant: distance)
-        let top = NSLayoutConstraint(item: self, attribute: .Top, relatedBy: .Equal, toItem: superview, attribute: .Top, multiplier: 1, constant: distance)
-        let bottom = NSLayoutConstraint(item: self, attribute: .Bottom, relatedBy: .Equal, toItem: superview, attribute: .Bottom, multiplier: 1, constant: distance)
+        let left = NSLayoutConstraint(item: self, attribute: .Left, toItem: superview, constant: distance)
+        let right = NSLayoutConstraint(item: self, attribute: .Right, toItem: superview, constant: distance)
+        let top = NSLayoutConstraint(item: self, attribute: .Top, toItem: superview, constant: distance)
+        let bottom = NSLayoutConstraint(item: self, attribute: .Bottom, toItem: superview, constant: distance)
         return [left, right, top, bottom]
     }
 
     // MARK: Same dimension
 
-    func matchDimension(dimension: NSLayoutAttribute, otherView: UIView) -> NSLayoutConstraint {
+    func matchDimension(dimension: NSLayoutAttribute, toItem view: UIView) -> NSLayoutConstraint {
         assert(dimension == .Width || dimension == .Height, "You can match the width or the height of a view, no other attribute is accepted")
-        return NSLayoutConstraint(item: self, attribute: dimension, relatedBy: .Equal, toItem: otherView, attribute: dimension, multiplier: 1, constant: 0)
+        return NSLayoutConstraint(item: self, attribute: dimension, toItem: view)
     }
 
     func matchHeightAndWidth() -> NSLayoutConstraint {
@@ -64,5 +64,25 @@ extension UIView {
         let width = NSLayoutConstraint(item: self, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: size.width)
         let height = NSLayoutConstraint(item: self, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: size.height)
         return [width, height]
+    }
+}
+
+extension NSLayoutConstraint {
+
+    // same attribute, relation = .Equal, multiplier = 1, constant = 0
+    convenience init(item view1: AnyObject, attribute attr: NSLayoutAttribute, relatedBy relation: NSLayoutRelation = .Equal, toItem view2: AnyObject?, multiplier: CGFloat = 1, constant c: CGFloat = 0) {
+        self.init(item: view1, attribute: attr, relatedBy: relation, toItem: view2, attribute: attr, multiplier: multiplier, constant: c)
+    }
+
+    class func constraintsWithVisualFormat(format: String, metrics: [NSObject : AnyObject]?, views: [NSObject : AnyObject]) -> [AnyObject] {
+        return NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(0), metrics: metrics, views: views)
+    }
+
+    class func constraintsWithVisualFormat(format: String, options opts: NSLayoutFormatOptions, views: [NSObject : AnyObject]) -> [AnyObject] {
+        return NSLayoutConstraint.constraintsWithVisualFormat(format, options: opts, views: views)
+    }
+
+    class func constraintsWithVisualFormat(format: String, views: [NSObject : AnyObject]) -> [AnyObject] {
+        return NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(0), metrics: nil, views: views)
     }
 }
